5de1c3fecf1a906b2e414b7b5048d79db5434db5
masin007 <marcin.a.ciesielski@gmail.com>
2019-01-07 20:33:37 +0100

    panda

--- web_apps/biblioteka/books/views.py
+++ web_apps/biblioteka/books/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
 # Create your views here.
+from books.models import Book
+
+
+def books_list(request):
+    books = Book.objects.all()
+    return render (
+        template_name="books/books_list.html",
+        context = {'books': books},
+        request = request
+    )
